0c871c81db4611270687c3a33ba88c424d369fde
Karol Preiskorn <karol.preiskorn@ibm.com>
2021-12-30 21:23:51 +0100

    correct settings bestaudio+bestvideo(<=640)

--- yt.sh
+++ yt.sh
@@ -19,10 +19,6 @@ if (( $# < 1 )); then
     exit 1
 fi
 
-# todo check -> created folder 'download' (or create it) downaload there all videos
-# todo check ffmpeg instaled?
-# todo chceck python3 is isnstalled?
-
 FILE=/usr/local/bin/youtube-dl
 
 if [ ! -x "$FILE" ]; then
@@ -31,35 +27,25 @@ if [ ! -x "$FILE" ]; then
 fi
 
 if [[ $1 =~ ^https://(www\.)?youtube\.com/watch\?.*v=([a-zA-Z0-9-]+).* ]]; then
+
     echo -e "${RED}Link $1 from YT convert to mkv & mp3${NC}"
-    /usr/local/bin/youtube-dl --format '(mp4)[height<=480]+bestaudio' --restrict-filenames --add-metadata --merge-output-format mkv --output "%(title)s.%(ext)s" $1
-    /usr/local/bin/youtube-dl --format bestaudio --extract-audio --audio-quality 0 --restrict-filenames --add-metadata --output "%(title)s.%(ext)s" $1
+
+    /usr/local/bin/youtube-dl -f 'bestvideo[height<=640]+bestaudio/best[height<=640]' --restrict-filenames --add-metadata --merge-output-format mkv --output "%(title)s.%(ext)s" $1
+    /usr/local/bin/youtube-dl --format bestaudio --extract-audio --audio-quality 0 --restrict-filenames --add-metadata --embed-thumbnail --output "%(title)s.%(ext)s" $1
 
 elif [[ $1 =~ ^https://(www\.)?youtube\.com/playlist\?list=.* ]]; then
+
     echo -e "${RED}Link $1 playlist from YT convert to mkv${NC}"
-    /usr/local/bin/youtube-dl --format '(mp4)[height<=480]+bestaudio' --sleep-interval 5 --ignore-errors  --restrict-filenames --add-metadata --merge-output-format mkv --output "%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s" $1
+
+    /usr/local/bin/youtube-dl -f 'bestvideo[height<=640]+bestaudio/best[height<=640]' --sleep-interval 5 --max-sleep-interval 12 --ignore-errors --restrict-filenames --add-metadata --merge-output-format mkv --output "%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s" $1
 
 elif [[ $1 =~ ^https://(www\.)?youtube\.com/playlist\?list=.* && $2 = "mp3" ]]; then
+
     echo -e "${RED}Link $1 playlist from YT convert to mp3${NC}"
+
     /usr/local/bin/youtube-dl --ignore-errors --audio-format mp3 --format bestaudio --extract-audio --audio-quality 0 --restrict-filenames --add-metadata --embed-thumbnail --output "%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s" $1
 
 else
     echo -e "${RED}Link $1 *NOT* from YT${NC}"
     exit 1
 fi
-
-
-# Videos can be filtered by their upload date using the options --date, --datebefore or --dateafter. They accept dates in two formats:
-
-#     Absolute dates: Dates in the format YYYYMMDD.
-#     Relative dates: Dates in the format (now|today)[+-][0-9](day|week|month|year)(s)?
-
-
-# Download playlist
-# youtube-dl -ict --yes-playlist --extract-audio --audio-format mp3 --audio-quality 0 https://www.youtube.com/playlist?list=UUCvVpbYRgYjMN7mG7qQN0Pg
-
-# Download playlist, --download-archive downloaded.txt add successfully downloaded files into downloaded.txt
-# youtube-dl --download-archive downloaded.txt --no-overwrites -ict --yes-playlist --extract-audio --audio-format mp3 --audio-quality 0 --socket-timeout 5 https://www.youtube.com/playlist?list=UUCvVpbYRgYjMN7mG7qQN0Pg
-
-# Retry until success, no -i option
-# while ! youtube-dl --download-archive downloaded.txt --no-overwrites -ct --yes-playlist --extract-audio --audio-format mp3 --audio-quality 0 --socket-timeout 5 <YT_PlayList_URL>; do echo DISCONNECTED; sleep 5; done
