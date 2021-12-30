aacfa30bb4a999e0ecfd73a6472328efdba2cd46
Karol Preiskorn <karol.preiskorn@ibm.com>
2021-12-30 18:52:18 +0100

    update after download paylist

--- yt.sh
+++ yt.sh
@@ -1,36 +1,50 @@
 # yt-video.sh
-# =================================================
-# --recode-video mkv
-# --audio-format best
-# -- 
+#
+# Prerequsition:
+#
+# - install youtube-dl from ```sudo curl -L https://yt-dl.org/downloads/latest/youtube-dl -o /usr/local/bin/youtube-dl && sudo chmod a+rx /usr/local/bin/youtube-dl````
+# - source: https://github.com/ytdl-org/youtube-dl
+# - ```sudo apt install ffmpeg```
+#
 # test: https://www.youtube.com/watch?v=GxrPn7qwt6c
 
+RED="\033[97;45m"
+NC="\033[0m" # No Color
+BLUE="\033[30;106m"
+GREEN="\033[1;37;42m"
 
 # Check that there are at least two arguments given in a bash script
 if (( $# < 1 )); then
-    echo 'yt-video.sh need at last one parameter'
+    echo -en "${RED}yt-video.sh need at last one parameter{NC}"
     exit 1
 fi
 
+# todo check -> created folder 'download' (or create it) downaload there all videos
+# todo check ffmpeg instaled?
+# todo chceck python3 is isnstalled?
 
 FILE=/usr/local/bin/youtube-dl
+
 if [ ! -x "$FILE" ]; then
-    echo "This $FILE is not exist please install"
+    echo -e "This $FILE is not exist please install"
     exit 1
 fi
 
 if [[ $1 =~ ^https://(www\.)?youtube\.com/watch\?.*v=([a-zA-Z0-9-]+).* ]]; then
-    echo "Link $1 from YT convert to mkv & mp3"
-    youtube-dl --format '(mp4)[height<=480]+bestaudio' --restrict-filenames --add-metadata --merge-output-format mkv --output "%(title)s.%(ext)s" $1
-    youtube-dl --format bestaudio --extract-audio --audio-quality 0 --restrict-filenames --add-metadata --output "%(title)s.%(ext)s" $1
+    echo -e "${RED}Link $1 from YT convert to mkv & mp3${NC}"
+    /usr/local/bin/youtube-dl --format '(mp4)[height<=480]+bestaudio' --restrict-filenames --add-metadata --merge-output-format mkv --output "%(title)s.%(ext)s" $1
+    /usr/local/bin/youtube-dl --format bestaudio --extract-audio --audio-quality 0 --restrict-filenames --add-metadata --output "%(title)s.%(ext)s" $1
+
 elif [[ $1 =~ ^https://(www\.)?youtube\.com/playlist\?list=.* ]]; then
-    echo "Link $1 playlist from YT convert to mkv"
-    youtube-dl --ignore-errors --dateafter 20200501 --format '(mp4)[height<=480]+bestaudio' --restrict-filenames --add-metadata --merge-output-format mkv --output "%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s" $1
-elif [[ $1 =~ ^https://(www\.)?youtube\.com/playlist\?list=.* ]]; then
-    echo "Link $1 playlist from YT convert to mp3"
-    youtube-dl --ignore-errors --audio-format mp3 --format bestaudio --extract-audio --audio-quality 0 --restrict-filenames --add-metadata --embed-thumbnail --output "%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s" $1
+    echo -e "${RED}Link $1 playlist from YT convert to mkv${NC}"
+    /usr/local/bin/youtube-dl --format '(mp4)[height<=480]+bestaudio' --sleep-interval 5 --ignore-errors  --restrict-filenames --add-metadata --merge-output-format mkv --output "%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s" $1
+
+elif [[ $1 =~ ^https://(www\.)?youtube\.com/playlist\?list=.* && $2 = "mp3" ]]; then
+    echo -e "${RED}Link $1 playlist from YT convert to mp3${NC}"
+    /usr/local/bin/youtube-dl --ignore-errors --audio-format mp3 --format bestaudio --extract-audio --audio-quality 0 --restrict-filenames --add-metadata --embed-thumbnail --output "%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s" $1
+
 else
-    echo "Link $1 *NOT* from YT"
+    echo -e "${RED}Link $1 *NOT* from YT${NC}"
     exit 1
 fi
 
