1c3d1a61048735559846340d74ef61eca17b7de8
C2RLO <karol-preiskorn@users.noreply.github.com>
2024-12-26 18:49:03 +0100

    Refactors yt.sh script for improved usability and maintainability
    Updates program name and installation instructions
    Adds safety measures and error handling
    Improves progress and error logging
    Enhances command-line argument parsing and usage display

---
+++ yt-playlist-et.sh
@@ -0,0 +1,3 @@
+youtube-dl -o "%%(playlist)s/%%(playlist_index)s - %%(title)s.%%(ext)s" --a https://www.youtube.com/watch?v=dTFDfR47dl4&list=PLEguIFSuqm6gBp5-ep8t-YjMrLAtMfYCt
+
+youtube-dl -o "%%(playlist)s/%%(playlist_index)s - %%(title)s.%%(ext)s" --a https://www.youtube.com/watch?v=nBXpFbOPUdA&list=PLEguIFSuqm6jDytL0SmF_gsf_szOPDs0u
--- yt.sh
+++ yt.sh
@@ -1,16 +1,22 @@
 #!/usr/bin/env bash
 #
-# PROGRAM: yt-video.sh
+# PROGRAM: yt.sh
 #
-# INSTALATION:
-# - install youtube-dl from ```sudo curl -L https://yt-dl.org/downloads/latest/youtube-dl -o /usr/local/bin/youtube-dl && sudo chmod a+rx /usr/local/bin/youtube-dl````
-# - source: https://github.com/ytdl-org/youtube-dl
-# - ```sudo apt install ffmpeg```
+# PRE-INSTALATION:
+#
+#  - apt install yt-dlp
+#  - sudo apt install ffmpeg
 #
 # TEST:
 # - https://www.youtube.com/watch?v=GxrPn7qwt6c
+#
+
+# More safety, by turning some bugs into errors.
+set -o errexit -o pipefail -o noclobber -o nounset
 
 v_date=$(date +"%Y-%m-%d")
+
+START=$(date +%s)
 
 RED='\033[7;49;31m'
 NC='\033[0m'
@@ -18,90 +24,114 @@ BLUE='\033[0;49;36m'
 VIOLET='\033[35;49m'
 GREY='\033[37;2m'
 
-START=$(date +%s)
-
 print_progress() {
-
 	END=$(date +%s)
-
 	lv_diff=$((END - START))
 	lv_diff_d=$((lv_diff / (3600 * 24)))
 	lv_diff_h=$((lv_diff / 3600 % 24))
 	lv_diff_m=$((lv_diff / 60 % 60))
 	lv_diff_s=$((lv_diff % 60))
-
-	echo -e "${BLUE}[progress] ${lv_diff_d} days ${lv_diff_h}h:${lv_diff_m}m:${lv_diff_s}s  -  $1${NC}"
+	echo -en "${BLUE}[yt.sh]${NC} ${lv_diff_h}h:${lv_diff_m}m:${lv_diff_s}s -  $1"
 }
 
 print_info() {
 	if [ "$#" -ne 1 ]; then
-		print_error "${RED}Illegal number of parameters f. print_info${NC}"
+		print_error "${RED}Illegal number of parameters f. print_info().${NC}"
 		exit 1
 	fi
 	TXT_COLOR="${GREY}${1}${NC} "
-	echo -e "${TXT_COLOR}"
+	echo -en "${TXT_COLOR}"
 }
 
 print_error() {
 	if [ "$#" -ne 1 ]; then
-		print_error "${RED}Illegal number of parameters f. print_info${NC}"
+		print_error "${RED}Illegal number of parameters f. print_error().${NC}"
 		exit 1
 	fi
 	TXT_COLOR="${RED}${1}${NC} "
-	echo -e "${TXT_COLOR}"
+	echo -en "${TXT_COLOR}"
 }
 
 print_debug() {
 	if [ "$#" -ne 1 ]; then
-		print_error "Illegal number of parameters f. print_info${NC}"
+		print_error "${VIOLET}Illegal number of parameters f. print_debug().${NC}"
 		exit 1
 	fi
 	TXT_COLOR="${VIOLET}${1}${NC} "
 	echo -e "${TXT_COLOR}"
 }
 
-# Check that there are at least two arguments given in a bash script
-if (($# < 1)); then
-	echo -en "$0 need at last one parameter from youtube video or playlist"
-	echo -en "       - second parametr $0 is oprional describe format output [mp3|flac|mkv]."
+
+FILE=/usr/bin/yt-dlp
+
+usage() {
+	echo -e ""
+	echo -e "Usage: yt.sh -a <yt url> -o [mp3|mkv]"
+	echo -e
+	echo -e "PRE-RUN:"
+	echo -e " - sudo apt install yt-dlp"
+	echo -e " - sudo apt install ffmpeg"
+	echo -e ""
+	echo -e "TEST:"
+	echo -e " - ./yt.sh https://www.youtube.com/watch?v=GxrPn7qwt6c"
+	echo -e ""
 	exit 1
+}
+
+regex='(https?|ftp|file)://[-[:alnum:]\+&@#/%?=~_|!:,.;]*[-[:alnum:]\+&@#/%=~_|]'
+#usage() { echo "Usage: $0 [-s <45|90>] [-p <string>]" 1>&2; exit 1; }
+u=""
+o=""
+while getopts "u:o:" opt; do
+	case "${opt}" in
+		u)
+			u=${OPTARG}
+			if [[ $u =~ $regex ]]
+			then
+				echo "Oki use URL: ${u}"
+			else
+				usage
+			fi
+			;;
+		o)
+			o=${OPTARG}
+			echo "Option audio specified ${o}."
+			;;
+		*)
+			usage
+			;;
+	esac
+
+done
+
+shift $((OPTIND-1))
+
+# echo "url = ${u}"
+# echo "out audio => ${o}"
+
+if [ -z "${u}" ] || [ -z "${o}" ]; then
+   usage
 fi
 
-FILE=/usr/bin/youtube-dl
-
-if [ ! -x "$FILE" ]; then
-	print_error "This $FILE is not exist please install"
-	exit 1
-fi
-
-if [[ $1 =~ ^https://(www\.)?youtube\.com/watch\?.*v=([a-zA-Z0-9-]+).* ]] && [ "$#" -eq 1 ]; then
-
-	print_progress "Link $1 from YT convert to mkv & mp3."
-
-	$FILE -f 'bestvideo[height<=640]+bestaudio/best[height<=640]' --restrict-filenames --add-metadata --merge-output-format mkv --output "%(title)s.%(ext)s" "$1"
-	$FILE --format bestaudio --extract-audio --audio-quality 0 --restrict-filenames --add-metadata --embed-thumbnail --output "%(title)s.%(ext)s" "$1"
-
-elif [[ $1 =~ ^https://(www\.)?youtube\.com/playlist\?list=.* ]] && [ "$#" -eq 1 ]; then
-
-	print_progress "Link $1 playlist from YT convert to mkv${NC}"
-
-	$FILE -f 'bestvideo[height<=640]+bestaudio/best[height<=640]' --verbose --sleep-interval 5 --max-sleep-interval 12 --ignore-errors --restrict-filenames --add-metadata --merge-output-format mkv --output "%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s" "$1"
-
-elif [[ $1 =~ ^https://(www\.)?youtube\.com/playlist\?list=.* && $2 = "mp3" ]] && [ "$#" -eq 2 ]; then
-
-	print_progress "Link $1 playlist from YT convert to mp3"
-
-	$FILE --ignore-errors --audio-format mp3 --format bestaudio --extract-audio --audio-quality 0 --restrict-filenames --add-metadata --embed-thumbnail --output "%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s" "$1"
-
-elif [[ $1 =~ ^https://(www\.)?youtube\.com/watch\?.*v=([a-zA-Z0-9-]+).* && $2 = "mp3" ]] && [ "$#" -eq 2 ]; then
-
-	print_progress "Link $1 video from YT convert to mp3"
-
-	$FILE --ignore-errors --audio-format mp3 --format bestaudio --extract-audio --audio-quality 0 --restrict-filenames --add-metadata --embed-thumbnail --output "%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s" "$1"
-
+if [[ "${u}" =~ ^https://(www\.)?youtube\.com/watch\?.*v=([a-zA-Z0-9-]+).* ]]; then
+	print_progress "Link ${u} from YT convert to mkv & mp3 single file."
+	if [[ $o == mkv ]]; then
+		$FILE -f 'bestvideo[height<=640]+bestaudio/best[height<=640]' --restrict-filenames --add-metadata --merge-output-format mkv --output "%(title)s.%(ext)s" "${u}"
+	else
+		$FILE --audio-format mp3 --extract-audio --audio-quality 0 --restrict-filenames --add-metadata --embed-thumbnail --output "%(title)s.%(ext)s" "${u}"
+	fi
+elif [[ "${u}" =~ ^https://(www\.)?youtube\.com/playlist\?list=.* ]]; then
+	print_progress "Link ${u} playlist from YT convert to mkv${NC}"
+	$FILE -f 'bestvideo[height<=640]+bestaudio/best[height<=640]' --verbose --sleep-interval 5 --max-sleep-interval 12 --ignore-errors --restrict-filenames --add-metadata --merge-output-format mkv --output "%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s" "${u}"
+elif [[ "${u}" =~ ^https://(www\.)?youtube\.com/playlist\?list=.* && $o = "mp3" ]]; then
+	print_progress "Link ${u} playlist from YT convert to mp3"
+	$FILE --ignore-errors --audio-format mp3 --format bestaudio --extract-audio --audio-quality 0 --restrict-filenames --add-metadata --embed-thumbnail --output "%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s" "${u}"
+elif [[ ${u} =~ ^https://(www\.)?youtube\.com/watch\?.*v=([a-zA-Z0-9-]+).* && $o = "mp3" ]]; then
+	print_progress "Link ${u} video from YT convert to mp3"
+	$FILE --ignore-errors --audio-format mp3 --format bestaudio --extract-audio --audio-quality 0 --restrict-filenames --add-metadata --embed-thumbnail --output "%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s" "${u}"
 else
-	print_error "[error] in call $0 link $1 *NOT* from YT video or playlist"
+	print_error "[error] in call $0 link ${u} *NOT* from YT video or playlist."
 	exit 1
 fi
 
-print_progress "Finish download $1 from YT"
+print_progress "Finish download ${u} from YT"
